825a48e6193ff26c50668f51b145ae70619d660b
A1eksX <119618024+A1eksX@users.noreply.github.com>
2023-04-07 18:27:41 +0300

    Add files via upload

---
+++ DzSeminar7.py
@@ -0,0 +1,115 @@
+# Задача 34:  Винни-Пух попросил Вас посмотреть, есть ли в его стихах ритм.
+# Поскольку разобраться в его кричалках не настолько просто, насколько легко он их придумывает,
+# Вам стоит написать программу.
+
+# Винни-Пух считает, что ритм есть, если число слогов (т.е. число гласных букв)
+# в каждой фразе стихотворения одинаковое. Фраза может состоять из одного слова, если во фразе несколько слов,
+# то они разделяются дефисами. Фразы отделяются друг от друга пробелами.
+
+# Стихотворение  Винни-Пух вбивает
+# в программу с клавиатуры. В ответе напишите “Парам пам-пам”, если с ритмом все в порядке и “Пам парам”,
+# если с ритмом все не в порядке
+#
+# *Пример:*
+#
+# **Ввод:** пара-ра-рам       рам-пам-папам           па-ра-па-да   # 3фразы
+# **Вывод:** Парам пам-пам
+
+# vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+# poem = input('Введите стихотворение: ').split()   # Тарам-парам пурум-бурум пара-ра-рам
+# listPhrases = []    # список фраз
+# listVowels = []     # список гласных
+# numberVowels = 0    # кол-во гласных
+# count = 0
+# for phrase in poem:
+#     listPhrases.append(phrase)
+#     for i in str(phrase):
+#         for j in vowels:
+#             if j == i:
+#                 count += 1
+#     numberVowels = count
+#     count = 0
+#     listVowels.append(numberVowels)
+# for numbers in range(len(listVowels)):
+#     count += listVowels[numbers]
+# proverka = count // len(listVowels)
+# if proverka != listVowels[0]:
+#     print('Пам парам')  # bad
+# else:
+#     print('Парам пам-пам')  # good
+
+#----------------------------------------------------------------------------------------------------------
+# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца.
+
+# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны.
+# Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля).
+# Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например,
+# у операции умножения.
+#
+# *Пример:*
+# **Ввод:** `print_operation_table(lambda x, y: x * y) `
+# **Вывод:**
+# 1 2 3 4 5 6
+#
+# 2 4 6 8 10 12
+# 3 6 9 12 15 18
+# 4 8 12 16 20 24
+# 5 10 15 20 25 30
+# 6 12 18 24 30 36
+
+# Вариант 1
+def operation(num_rows, num_columns):
+    for i in range(1, num_rows + 1):
+        for j in range(1, num_columns + 1):
+            table = i * j
+            print(table, end='\t')
+        print()
+
+def print_operation_table(operation, x, y):
+    print(operation(x, y))
+
+print_operation_table(operation, 6, 6)
+print_operation_table(lambda x, y: x * y, 3, 3)
+
+#Вариант 2
+# def print_operation_table(operation, num_rows=6, num_columns=6):
+#     for i in range(1, num_rows + 1):
+#         for j in range(1, num_columns + 1):
+#             table = i * j
+#             print(table, end='\t')
+#         print()
+#     print(operation(num_rows, num_columns))
+#
+# print_operation_table(lambda x, y: x * y)
+
+# ----------------------------------------------------------------------------------
+# def show_table(table: list[list[int]]) -> None:
+#     '''Просто красиво печает матрицу)'''
+#     print('\n'.join('\t'.join(map(str, row)) for row in table))
+#
+#
+# def print_operation_table(oper: callable,
+#                           num_columns: int = 4,
+#                           num_rows: int = 4) -> None:
+#     '''Выводит таблицу для чисел с заданной оперцией oper,
+#     числом столбцов num_columns и строк num_rows'''
+#     table = [list(range(i, i+num_columns)) for i in range(1, num_rows+1)]
+#     # show_table(table)
+#     for i in range(1, len(table)):
+#         for j in range(1, len(table[i])):
+#             table[i][j] = oper(table[i][0], table[0][j])
+#     show_table(table)
+
+# ---------------------------------------------------------------------
+# def print_operation_table(operation, num_rows=6, num_columns=6):
+#     for i in range(1, num_rows + 1):
+#         for j in range(1, num_columns + 1):
+#             if i != 1 and j != 1:
+#                 print(operation(j, i), end="\t")
+#             elif i == 1:
+#                 print(j, end="\t")
+#             else:
+#                 print(i, end="\t")
+#             print()
+# print_operation_table(lambda x, y: x + y)
